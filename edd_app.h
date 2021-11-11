769a313c593cec545a3026e7d633dda0bbe925a3
Madison Krell <55954812+makrell38@users.noreply.github.com>
2021-11-11 11:09:46 -0600

    Update and rename edd-app.cpp to edd_app.h
    Make header file

--- edd_app.h
+++ edd_app.h
@@ -1,3 +1,6 @@
+#ifndef EDD_APP
+#define EDD_APP
+
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -7,10 +10,10 @@
 #include <limits.h>
 #include <queue>
 
+#include "graph.h"
 
 using namespace std;
 
-int delta = 9;
 
 struct path{
     vector<int> p;
@@ -24,41 +27,11 @@ struct CompareCost{
     }
 };
 
-
-void addEdge(int** graph, int v1, int v2, int weight){
-    graph[v1][v2] = weight;
-    graph[v2][v1] = weight;
-    return;
-}
-
-void printGraph(int** graph, int v){
-    for(int i =0; i <= v; i++){
-        cout << "[";
-        for(int j=0; j<=v; j++){
-            cout<< graph[i][j] << " ";
-        }
-        cout<<"]"<<endl;
-    }
-    cout<<endl;
-}
-
-void printPath(int **graph, int v){
-    for(int i=0; i<=v; i++){
-        for(int j=0; j<=v; j++){
-            if(graph[i][j] != 0){
-                cout<<i<<"->"<<j<<" ";
-            }
-        }
-        //cout<<endl;
-    }
-    cout<<endl;
-}
-
-void sendToNeighbors(int** graph, int v, int **Tms, int* R, int node, int* T){
+void sendToNeighbors(Graph* g, int **Tms, int node, int* T){
     queue<int> reached;
-    for(int i=1;i<=v;i++){
+    for(int i=1;i<=g->v;i++){
         if(i != node){
-            if(graph[node][i] != 0 && R[i-1] != 0){
+            if(g->graph[node][i] != 0 && g->R[i-1] != 0){
                 if(T[i-1] == 0){
                     Tms[node][i] = 1;
                     T[i-1] = 1;
@@ -71,30 +44,30 @@ void sendToNeighbors(int** graph, int v, int **Tms, int* R, int node, int* T){
     while(!reached.empty()){
         next = reached.front();
         reached.pop();
-        sendToNeighbors(graph,v,Tms,R,next, T);
+        sendToNeighbors(g, Tms, next, T);
     }
 }
 
-int ** CMST(int** graph, int *R, int v){
-    int** Tms= new int*[v+1];
-    for(int i=0; i<=v; i++){
-        Tms[i] = new int[v+1];
+int ** CMST(Graph* g){
+    int** Tms= new int*[g->v+1];
+    for(int i=0; i<=g->v; i++){
+        Tms[i] = new int[g->v+1];
     }
-    int T[v] = {0};
+    int T[g->v] = {0};
 
     bool check = false;
     while(!check){
-        int con[v] = {0};
-        for(int i=1; i<=v; i++){
-            for(int j=1; j<=v; j++){
-                if(graph[i][j] != 0 && Tms[i][j] == 0 && R[j-1] != 0){
+        int con[g->v] = {0};
+        for(int i=1; i<=g->v; i++){
+            for(int j=1; j<=g->v; j++){
+                if(g->graph[i][j] != 0 && Tms[i][j] == 0 && g->R[j-1] != 0){
                     con[i-1]++;
                 }
             }
         }
         int max = 0;
         int hold;
-        for(int i=0; i<v; i++){
+        for(int i=0; i<g->v; i++){
             if(con[i] > max){
                 max = con[i];
                 hold = i+1;
@@ -102,20 +75,20 @@ int ** CMST(int** graph, int *R, int v){
         }
         Tms[0][hold] = 1;
         T[hold-1] = 1;
-        sendToNeighbors(graph, v, Tms, R, hold, T);
+        sendToNeighbors(g, Tms, hold, T);
 
         check = true;
-        for(int i=0;i<v;i++){
-            if(R[i] != 0 && T[i] == 0)
+        for(int i=0;i<g->v;i++){
+            if(g->R[i] != 0 && T[i] == 0)
                 check = false;
         }
     }
     return Tms;
 }
 
-path dijkstraClone(int **graph, int v, int v1, int v2){
-    int dist[v+1] = {INT_MAX};
-    bool set[v+1] = {false};
+path dijkstraClone(Graph* g, int v1, int v2){
+    int dist[g->v+1] = {INT_MAX};
+    bool set[g->v+1] = {false};
     dist[v1] = 0;
     priority_queue<path, vector<path>, CompareCost> q;
 
@@ -130,8 +103,8 @@ path dijkstraClone(int **graph, int v, int v1, int v2){
         q.pop();
         if(check.p.back() == v2)
             return check;
-        for(int i=1; i<=v; i++){
-            if(graph[check.p.back()][i] != 0){
+        for(int i=1; i<=g->v; i++){
+            if(g->graph[check.p.back()][i] != 0){
                 path add;
                 add.p = check.p;
                 add.p.push_back(i);
@@ -142,24 +115,24 @@ path dijkstraClone(int **graph, int v, int v1, int v2){
     }
 }
 
-int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
+int ** EDDA(Graph* g, int **Tms){
     //initialize variables
-    int **Tedda = new int*[v+1];
-    int parents[v+1];
-    int d[v+1];
-    int cost[v+1];
+    int **Tedda = new int*[g->v+1];
+    int parents[g->v+1];
+    int d[g->v+1];
+    int cost[g->v+1];
 
     //set parents, cost, and d
-    for(int i=0; i<=v; i++){
+    for(int i=0; i<=g->v; i++){
         parents[i] = -1;
         cost[i] = INT_MAX;
         d[i] = 0;
     }
 
     // set Tedda to Tms and set parents[v] if v path in Tedda
-    for(int i=0;i<=v;i++){
-        Tedda[i] = new int[v+1];
-        for(int j=0;j<=v;j++){
+    for(int i=0;i<=g->v;i++){
+        Tedda[i] = new int[g->v+1];
+        for(int j=0;j<=g->v;j++){
             Tedda[i][j] = Tms[i][j];
             if(Tms[i][j] != 0){
                 parents[j] = i;
@@ -168,10 +141,10 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
     }
 
     // set cost[v] if v in Tedda
-    for(int i=1;i<=v;i++){
+    for(int i=1;i<=g->v;i++){
         if(parents[i] != -1){
             if(parents[i] == 0){
-                cost[i] = cost[0] + delta;
+                cost[i] = cost[0] + g->delta;
                 d[i] = 0;
 
             }
@@ -183,7 +156,7 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
     }
 
     // for each edge server in DFS order
-    bool visited[v+1] = {false};
+    bool visited[g->v+1] = {false};
     stack<int> st;
     st.push(0);
 
@@ -196,7 +169,7 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
 
         // if cloud node, add edges reached by cloud and continue
         if(s == 0){
-            for(int i =1; i<=v;i++){
+            for(int i =1; i<=g->v;i++){
                 if(Tedda[s][i] != 0){
                     if(!visited[i])
                         st.push(i);
@@ -205,9 +178,9 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
             continue;
         }
 
-        // if not a destination server or depth is already less than H, conitnue
-        if(R[s-1] == 0 || d[s] <= H){
-            for(int i =1; i<=v;i++){
+        // if not a destination server or depth is already less than dLimit, conitnue
+        if(g->R[s-1] == 0 || d[s] <= g->dLimit){
+            for(int i =1; i<=g->v;i++){
                 if(Tedda[s][i] != 0){
                     if(!visited[i])
                         st.push(i);
@@ -219,9 +192,9 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
         //find min cost path [c-s-v]
         int min = std::numeric_limits<int>::max();
         path t;
-        for(int i=1;i<=v;i++){
+        for(int i=1;i<=g->v;i++){
             if(Tedda[0][i] != 0){
-                path temp = dijkstraClone(graph, v, i, s);
+                path temp = dijkstraClone(g, i, s);
                 if(temp.p.size() != 0){
                     if(temp.p.size()-1 < min){
                         min = temp.p.size()-1;
@@ -231,8 +204,8 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
             }
         }
 
-        // if hops is less than H, update for all edges in the path
-        if(min <= H){
+        // if hops is less than g->dLimit, update for all edges in the path
+        if(min <= g->dLimit){
             for(std::vector<int>::size_type it = 0; it != t.p.size(); ++it){
                 parents[t.p[it]] = t.p[it-1];
                 if(t.p[it-1] != 0){
@@ -240,7 +213,7 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
                     d[t.p[it]] = 0;
                 }
                 else{
-                    cost[t.p[it]] = cost[t.p[it-1]] +delta;
+                    cost[t.p[it]] = cost[t.p[it-1]] + g->delta;
                     d[t.p[it]] = d[t.p[it-1]] + 1;
                 }
                 Tedda[t.p[it-1]][t.p[it]] = 1;
@@ -248,10 +221,10 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
             }
 
             // update shortest cost path for each node in R
-            for(int i=1; i<=v; i++){
+            for(int i=1; i<=g->v; i++){
                 if(i != s){
-                    if(R[i-1] != 0){
-                        path t2 = dijkstraClone(graph, v, s, i);
+                    if(g->R[i-1] != 0){
+                        path t2 = dijkstraClone(g, s, i);
                         if(t2.cost != INT_MAX){
                             if(cost[i] > cost[s] + t2.cost){
                                 cost[i] = cost[s] + t2.cost;
@@ -268,15 +241,15 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
         // If depth limit is still exceeded, add path from cloud to s
         else{
             parents[s] = 0;
-            cost[s] = delta;
+            cost[s] = g->delta;
             d[s] = 0;
             Tedda[0][s] = 1;
         }
 
         // Updated shortest cost path and parents for each child of s
-        for(int i=1; i<=v; i++){
+        for(int i=1; i<=g->v; i++){
             if(Tedda[s][i] != 0){
-                path t3 = dijkstraClone(graph, v, s, i);
+                path t3 = dijkstraClone(g, s, i);
                 if(cost[i] > cost[s] + t3.cost){
                     parents[i] = s;
                     cost[i] = cost[s] + t3.cost;
@@ -285,7 +258,7 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
                     parents[s] = i;
                     cost[s] = cost[i] + t3.cost;
                 }
-                for(int j=1; j<=v; j++){
+                for(int j=1; j<=g->v; j++){
                     if(parents[j] == 0){
                         d[j] = 0;
                     }
@@ -296,7 +269,7 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
         }
 
         // for visiting in DFS order
-        for(int i =1; i<=v;i++){
+        for(int i =1; i<=g->v;i++){
             if(Tedda[s][i] != 0){
                 if(!visited[i]){
                     st.push(i);
@@ -307,8 +280,8 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
     }
 
     // Prune unused edges in Tedda by parents[]
-    for(int i=0; i<=v; i++){
-        for(int j=0; j<=v; j++){
+    for(int i=0; i<=g->v; i++){
+        for(int j=0; j<=g->v; j++){
             if(Tedda[i][j] != 0){
                 if(parents[j] != i)
                     Tedda[i][j] = 0;
@@ -320,39 +293,4 @@ int ** EDDA(int **graph, int* R, int c, int H, int **Tms, int v){
     return Tedda;
 }
 
-int main(){
-    int v = 10;
-    int **graph = new int*[v+1];
-    for(int i=0; i<=v; i++)
-        graph[i] = new int[v+1];
-
-    addEdge(graph, 5,8,1);
-    addEdge(graph, 5,2,1);
-    addEdge(graph, 9,8,1);
-    addEdge(graph, 7,8,1);
-    addEdge(graph, 10,9,1);
-    addEdge(graph, 6,9,1);
-    addEdge(graph, 6,7,1);
-    addEdge(graph, 3,7,1);
-    addEdge(graph, 6,3,1);
-    addEdge(graph, 1,3,1);
-    addEdge(graph, 1,4,1);
-    addEdge(graph, 4,10,1);
-    addEdge(graph, 2,4,1);
-    addEdge(graph, 1,2,1);
-
-    for(int i = 1; i <= v; i++)
-        addEdge(graph, 0, i, delta);
-    //printGraph(graph, v);
-    int R[v] = {1,1,1,1,0,1,1,0,1,0};
-    int **Tms = CMST(graph, R, v);
-    //printGraph(Tms, v);
-    int **Tedda = EDDA(graph, R, 0, 1, Tms, v);
-    //printGraph(Tedda, v);
-    cout<<"Tms path: "<<endl;
-    printPath(Tms, v);
-    cout<<"Tedda path: "<<endl;
-    printPath(Tedda, v);
-
-    return 0;
-}
+#endif
